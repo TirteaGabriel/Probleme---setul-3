8c4aa4b73fa871aafea9bcd1c908c204fe6e9fca
TirteaGabriel <gabrieltirtea@gmail.com>
2023-01-10 19:05:22 +0200

    +probleme

--- Probleme - setul 3/Program.cs
+++ Probleme - setul 3/Program.cs
@@ -8,6 +8,17 @@ namespace Probleme___setul_3
 {
     internal class Program
     {
+        static int[] Test(int n)
+        {
+            Random rnd = new Random();
+            int[] A = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                A[i] = rnd.Next(1, 100);
+                Console.Write($"{A[i]} ");
+            }
+            return A;
+        }
         static void p1()
         {
             int n = int.Parse(Console.ReadLine());
@@ -45,18 +56,170 @@ namespace Probleme___setul_3
         static void p3()
         {
             //Pentru extra-credit realizati programul efectuand 3n/2 comparatii (in cel mai rau caz). 
+            int i;
             int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            string[] t = Console.ReadLine().Split(' ');
-            for (int i = 0; i < n; i++)
+            int[] A = Test(n);
+            /*int[] A = new int[n];*/
+            int max, min, poz_max, poz_min;
+            int comparatii = 0;
+            if (n % 2 == 0)
             {
-                v[i] = int.Parse(t[i]);
+                if (A[0] > A[1])
+                {
+                    max = A[0];
+                    min = A[1];
+                    poz_max = 0;
+                    poz_min = 1;
+                }
+                else
+                {
+                    min = A[0];
+                    max = A[1];
+                    poz_min = 0;
+                    poz_max = 1;
+                }
+                i = 2;
+                comparatii += 2;
             }
-            int maxim = int.MinValue;
-            int minim = int.MaxValue;
+            else
+            {
+                min = A[0];
+                max = A[0];
+                poz_min = 0;
+                poz_max = 0;
+                i = 1;
+                comparatii++;
+            }
+
+            while (i < n - 1)
+            {
+                if (A[i] > A[i + 1])
+                {
+                    if (A[i] > max)
+                    {
+                        max = A[i];
+                        poz_max = i;
+                    }
+                    if (A[i + 1] < min)
+                    {
+                        min = A[i + 1];
+                        poz_min = i + 1;
+                    }
+                    comparatii += 3;
+                }
+                else
+                {
+                    if (A[i + 1] > max)
+                    {
+                        max = A[i + 1];
+                        poz_max = i + 1;
+                    }
+                    if (A[i] < min)
+                    {
+                        min = A[i];
+                        poz_min = i;
+                    }
+                    comparatii += 3;
+                }
+                i += 2;
+            }
+            Console.WriteLine($"Pozitia minimului este {poz_min}({min}), iar pozitia maximului este {poz_max}({max})");
+            Console.WriteLine($"Numarul de comparatii este {comparatii}");
+        }
+        static void p4()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] A = Test(n);
+            int max = A[0];
+            int min = A[0];
+            int aparmax = 1;
+            int aparmin = 1;
+            for(int i = 0; i < n; i++)
+            {
+                if (A[i] > max)
+                {
+                    max = A[i];
+                    aparmax = 1;
+                }
+                else if (A[i] == max)
+                    aparmax++;
+                if (A[i] < min)
+                {
+                    min = A[i];
+                    aparmin = 1;
+                }
+                else if (A[i] == min)
+                    aparmin++;
+            }
+            Console.WriteLine($"Numarul minim este {min} si apare de {aparmin} ori, iar numarul maxim este {max} si apare de {aparmax} ori");
+        }
+        static void p5()
+        {
+            Console.WriteLine("Introduceti lungimea vectorului:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea lui e:");
+            int e = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea lui k:");
+            int k = int.Parse(Console.ReadLine());
+            int[] A = Test(n);
+            int[] B = new int[n + 1];
+            for (int i = 0, j = 0; i < n && j <= n; i++, j++)
+            {
+                if (k != i)
+                    B[j] = A[i];
+                else
+                {
+                    B[j] = e;
+                    j++;
+                    B[j] = A[i];
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i <= n; i++)
+            {
+                Console.Write($"{B[i]} ");
+            }
+        }
+        static void p6()
+        {
+            Console.WriteLine("Introduceti lungimea vectorului:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea lui k:");
+            int k = int.Parse(Console.ReadLine());
+            int[] A = Test(n);
+            int[] B = new int[n - 1];
+            for (int i = 0, j = 0; i < n && j < n - 1; i++, j++)
+            {
+                if (k != i)
+                    B[j] = A[i];
+                else
+                {
+                    i++;
+                    B[j] = A[i];
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.Write($"{B[i]} ");
+            }
+        }
+        static void p7()
+        {
+            Console.WriteLine("Introduceti lungimea vectorului:");
+            int n = int.Parse(Console.ReadLine());
+            int[] A = Test(n);
+            int[] B = new int[n];
+            int j = 0;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                B[j] = A[i];
+                j++;
+            }
+            Console.WriteLine();
             for (int i = 0; i < n; i++)
             {
-                if()
+                Console.Write($"{B[i]} ");
             }
         }
         static void Main(string[] args)
@@ -77,27 +240,27 @@ namespace Probleme___setul_3
                 if (nrp == 3)
                 {
                     Console.WriteLine("Problema " + nrp);
-                    //p3();
+                    p3();
                 }
                 if (nrp == 4)
                 {
                     Console.WriteLine("Problema " + nrp);
-                    //p4();
+                    p4();
                 }
                 if (nrp == 5)
                 {
                     Console.WriteLine("Problema " + nrp);
-                    //p5();
+                    p5();
                 }
                 if (nrp == 6)
                 {
                     Console.WriteLine("Problema " + nrp);
-                    //p6();
+                    p6();
                 }
                 if (nrp == 7)
                 {
                     Console.WriteLine("Problema " + nrp);
-                    //p7();
+                    p7();
                 }
                 if (nrp == 8)
                 {
@@ -222,5 +385,6 @@ namespace Probleme___setul_3
                 Console.ReadKey();
             }
         }
+
     }
 }
